f8c919fd5b023feda0f1d88185b1b467ce513e7d
anagramiccoder <jdvidad31@gmail.com>
2023-04-21 08:28:43 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,98 @@
+use ArithCmpOp::*;
+use ArithExpr::*;
+use BinArithOp::*;
+use BinLogicOp::*;
+use BoolExpr::*;
+use Expr::*;
+use Value::*;
+
+pub enum Expr {
+    ArithExpr(ArithExpr),
+    BoolExpr(BoolExpr),
+}
+
+pub enum ArithExpr {
+    BinArithExpr {
+        left: Box<ArithExpr>,
+        right: Box<ArithExpr>,
+        op: BinArithOp,
+    },
+    IntLit(i64),
+}
+
+pub enum BoolExpr {
+    ArithCmpExpr {
+        left: Box<ArithExpr>,
+        right: Box<ArithExpr>,
+        op: ArithCmpOp,
+    },
+    BinBoolExpr {
+        left: Box<BoolExpr>,
+        right: Box<BoolExpr>,
+        op: BinLogicOp,
+    },
+    NotExpr(Box<BoolExpr>),
+    BoolLit(bool),
+}
+
+pub enum BinArithOp {
+    AddOp,
+    SubOp,
+    MulOp,
+    IntDivOp,
+}
+
+pub enum ArithCmpOp {
+    LtOp,
+    LteOp,
+    GtOp,
+    GteOp,
+    ArithEqOp,
+    ArithNeqOp,
+}
+
+pub enum BinLogicOp {
+    AndOp,
+    OrOp,
+    BoolEqOp,
+    BoolNeqOp,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Value {
+    BoolValue(bool),
+    IntValue(i64),
+}
+pub fn eval(expr: Expr) -> Value {
+    ...
+}
+
+pub fn eval_arith_expr(arith_expr: ArithExpr) -> i64 {
+    ...
+}
+
+pub fn eval_bool_expr(bool_expr: BoolExpr) -> bool {
+    ...
+}
+
+fn main() {}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    ...
+
+    #[test]
+    fn test_sample() {
+        let expr = BoolExpr(BoolLit(true));
+        let answer = BoolValue(true);
+
+        assert_eq!(eval(expr), answer);  // eval(BoolExpr(BoolLit(true))) == BoolValue(true)
+    }
+
+    #[test]
+    fn test_others() {
+        main();
+        println!("{:?}", BoolValue(true));
+    }
+}
